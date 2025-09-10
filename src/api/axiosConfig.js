0846b92a39d5e605e63a2fd8db7c5784d71f1611
dusoSK1 <dusanstanko123@gmail.com>
2025-09-10 10:45:22 +0200

    Update axiosConfig

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
-//Base URL configuration for axios
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8080";
+//Base url from environment variable or default to localhost
+const API_URL = process.env.REACT_APP_API_URL 
 
 const api = axios.create({
-    baseURL: API_URL,
+    baseURL: process.env.REACT_APP_API_URL,
     headers: {
         "Content-Type": "application/json"
-    }
+    },
+    withCredentials: true
 });
 
 export default api;
