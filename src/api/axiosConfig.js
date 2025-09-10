a463698ec8906251f7ff4525e6a925cded105314
dusoSK1 <dusanstanko123@gmail.com>
2025-09-10 15:55:12 +0200

    Update axiosConfig

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 //Base url from environment variable or default to localhost
-const API_URL = process.env.REACT_APP_API_URL || "https://insuranceapp-backend-latest.onrender.com/";
+const API_URL = process.env.REACT_APP_API_URL || "https://corsfix.com/proxy/https://insuranceapp-backend-latest.onrender.com/";
 
 const api = axios.create({
     baseURL: API_URL,
